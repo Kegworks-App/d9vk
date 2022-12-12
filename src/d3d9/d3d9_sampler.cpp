3d1e300f7d33497794d9faf64dd6ef37f608b4aa
Robin Kertels <robin.kertels@gmail.com>
2022-12-12 18:56:18 +0100

    [d3d9] Do not round sampler lod bias

--- src/d3d9/d3d9_sampler.cpp
+++ src/d3d9/d3d9_sampler.cpp
@@ -2,43 +2,43 @@
 
 namespace dxvk {
 
-  size_t D3D9SamplerKeyHash::operator () (const D3D9SamplerKey& key) const {
+  size_t D3D9SamplerKey::hash() const {
     DxvkHashState state;
 
     std::hash<DWORD>                dhash;
     std::hash<D3DTEXTUREADDRESS>    tahash;
     std::hash<D3DTEXTUREFILTERTYPE> tfhash;
-    std::hash<float>                fhash;
+    std::hash<int>                  ihash;
     std::hash<bool>                 bhash;
 
-    state.add(tahash(key.AddressU));
-    state.add(tahash(key.AddressV));
-    state.add(tahash(key.AddressW));
-    state.add(tfhash(key.MagFilter));
-    state.add(tfhash(key.MinFilter));
-    state.add(tfhash(key.MipFilter));
-    state.add(dhash (key.MaxAnisotropy));
-    state.add(fhash (key.MipmapLodBias));
-    state.add(dhash (key.MaxMipLevel));
-    state.add(dhash (key.BorderColor));
-    state.add(bhash (key.Depth));
+    state.add(tahash(AddressU));
+    state.add(tahash(AddressV));
+    state.add(tahash(AddressW));
+    state.add(tfhash(MagFilter));
+    state.add(tfhash(MinFilter));
+    state.add(tfhash(MipFilter));
+    state.add(dhash (MaxAnisotropy));
+    state.add(ihash (MipmapLodBias.data));
+    state.add(dhash (MaxMipLevel));
+    state.add(dhash (BorderColor));
+    state.add(bhash (Depth));
 
     return state;
   }
 
 
-  bool D3D9SamplerKeyEq::operator () (const D3D9SamplerKey& a, const D3D9SamplerKey& b) const {
-    return a.AddressU       == b.AddressU
-        && a.AddressV       == b.AddressV
-        && a.AddressW       == b.AddressW
-        && a.MagFilter      == b.MagFilter
-        && a.MinFilter      == b.MinFilter
-        && a.MipFilter      == b.MipFilter
-        && a.MaxAnisotropy  == b.MaxAnisotropy
-        && a.MipmapLodBias  == b.MipmapLodBias
-        && a.MaxMipLevel    == b.MaxMipLevel
-        && a.BorderColor    == b.BorderColor
-        && a.Depth          == b.Depth;
+  bool D3D9SamplerKey::eq(const D3D9SamplerKey& other) const {
+    return AddressU            == other.AddressU
+        && AddressV            == other.AddressV
+        && AddressW            == other.AddressW
+        && MagFilter           == other.MagFilter
+        && MinFilter           == other.MinFilter
+        && MipFilter           == other.MipFilter
+        && MaxAnisotropy       == other.MaxAnisotropy
+        && MipmapLodBias.data  == other.MipmapLodBias.data
+        && MaxMipLevel         == other.MaxMipLevel
+        && BorderColor         == other.BorderColor
+        && Depth               == other.Depth;
   }
 
 }
